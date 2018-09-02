22d4d692c3e3a870d2d780b08e997ff98bf33a49
Karolina Romanska <kika.romanska@gmail.com>
2018-09-02 15:48:54 +0200

    Changing button depend on authentication

--- src/components/LandingPage/Landing.js
+++ src/components/LandingPage/Landing.js
@@ -9,6 +9,8 @@ import {
 import './Landing.css';
 import { RedirectionButton} from "../Buttons/RedirectionButton";
 import img from '../images/imageedit_6_2261919941.png';
+import AuthUserContext from '../AuthUserContext';
+import MainButton from "../Buttons/MainButton";
 
 const LandingPage = () =>
     <Grid className="landingPage">
@@ -16,11 +18,19 @@ const LandingPage = () =>
             <Col xs={12} md={6} className="landingPage--text">
                 <h1 className='landingPage--text__header'>penny-pincher</h1>
                 <p className='landingPage--text__para'>Because money matters.</p>
-
-                <RedirectionButton
-                    type={'primary'}
-                    route={"SignUp"}
-                    title={"Try free for 20 days"}/>
+                <AuthUserContext.Consumer>
+                    {authUser=> authUser
+                        ?  <MainButton
+                        type={'primary'}
+                        title={"Learn more"}
+                        />
+                        : <RedirectionButton
+                            type={'primary'}
+                            route={"SignUp"}
+                            title={"Try free for 20 days"}
+                        />
+                    }
+                </AuthUserContext.Consumer>
             </Col>
             <Col xs={12} md={6} className="landingPage--image">
                 <Image src={img} responsive style={{ marginTop: '80px', display: 'block', marginLeft: 'auto', marginRight: 'auto', marginBottom: 'auto'}}/>
